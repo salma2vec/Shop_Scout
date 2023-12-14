323d8015e2e340801226cc9038e61698ef60cd74
Salma <salmasaa02@gmail.com>
2023-12-14 14:48:38 +0530

    chore: Update taglines in Hero.js for clearer messaging

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -3,27 +3,22 @@ import React, { useState } from "react";
 function Hero() {
   const [searchTerm, setSearchTerm] = useState("");
 
-<<<<<<< HEAD
-  
-=======
-
->>>>>>> 5f9f39945626a3edb1ff6f38c115b683346fed9c
   return (
     <div className="w-full h-screen flex items-center justify-center p-10 md:p-20">
       <div className="w-full ">
         <div className="flex flex-col gap-4 items-start justify-center w-full">
           <h1 className="font-bold text-4xl md:text-5xl leading-none max-w-2xl">
-            Compare prices of products from different websites
+            Elevate Your E-commerce Intelligence with ShopScout
           </h1>
           <p className="text-lg leading-tight md:text-xl">
-            Search for a product and compare prices from your favorite websites
+            An advanced price comparison tool designed to simplify the process of comparing product prices across various websites.
           </p>
           <div className="flex items-start md:items-center justify-center md:flex-row flex-col gap-4 mt-4 ">
             <div className="flex items-center justify-center ">
               <input
                 type="text"
                 onChange={(e) => setSearchTerm(e.target.value)}
-                placeholder="Find a perfect product"
+                placeholder="Find the Best Deals"
                 className=" py-3 px-6 !mb-0 rounded-full border text-black placeholder:text-gray-400 border-gray-300 focus:outline-none focus:ring-2 focus:ring-lightGreen focus:border-transparent"
               />
             </div>
@@ -32,7 +27,7 @@ function Hero() {
                 href="#products"
                 className="py-3 px-6 rounded-full bg-lightGreen text-white font-bold"
               >
-                Search
+                Search Now
               </a>
             </div>
           </div>
