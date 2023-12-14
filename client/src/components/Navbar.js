6138b1d59acc1a88fd6dd1de509c60d6399bb401
Salma <salmasaa02@gmail.com>
2023-12-14 18:52:03 +0530

    fix(navbar): Implement toggle with default animation properties

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,6 +3,40 @@ import LogoImg from "../assets/logo-no-background.png";
 import { DarkModeSwitch } from 'react-toggle-dark-mode';
 
 const Navbar = ({ toggleDarkMode }) => {
+  const defaultProperties = {
+    dark: {
+      circle: {
+        r: 9,
+      },
+      mask: {
+        cx: '50%',
+        cy: '23%',
+      },
+      svg: {
+        transform: 'rotate(40deg)',
+      },
+      lines: {
+        opacity: 0,
+      },
+    },
+    light: {
+      circle: {
+        r: 5,
+      },
+      mask: {
+        cx: '100%',
+        cy: '0%',
+      },
+      svg: {
+        transform: 'rotate(90deg)',
+      },
+      lines: {
+        opacity: 1,
+      },
+    },
+    springConfig: { mass: 4, tension: 250, friction: 35 },
+  };
+
   return (
     <div className="flex items-center justify-center px-20 py-5">
       <div className="flex items-center justify-between w-full">
