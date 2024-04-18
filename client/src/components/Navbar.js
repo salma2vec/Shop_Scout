10f097aa1e05abd1e6854ee4c09eb23fb324e852
Joel <70848930+raizestudio@users.noreply.github.com>
2024-04-18 18:15:25 +0200

    fix navbar sizing

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,16 +4,16 @@ import DarkModeToggle from './DarkModeToggle';
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-center px-20 py-5">
+    <div className="flex items-center justify-center px-8 py-2" style={}>
       <div className="flex items-center justify-between w-full">
-        <div className="w-24 rounded-full">
+        <div className="w-20 rounded-full">
           <img src={LogoImg} alt="logo" className="" />
         </div>
         <div className="flex items-center justify-center gap-4">
           <a href="/login" className="text-white hover:text-gray-400">
             Login
           </a>
-          <a href="/signup" className="text-white hover:text-gray-600 bg-lightGreen rounded-full px-4 py-1">
+          <a href="/signup" className="px-4 py-1 text-white rounded-full hover:text-gray-600 bg-lightGreen">
             Sign Up
           </a>
           <DarkModeToggle />
--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import './DarkModeToggle.css'; // Import the DarkModeToggle.css file
-import ComparisonForm from './components/ComparisonForm';
-import ProductResults from './components/ProductResults';
 
 ReactDOM.render(
   <React.StrictMode>
